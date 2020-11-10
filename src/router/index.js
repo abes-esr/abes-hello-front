9c04c42122598f570da8dd6ead5b2fdfef17b055
Duy <56829213+4duytran@users.noreply.github.com>
2020-11-10 10:36:27 +0100

    Init la nouvelle version de Vue CLI avec le plugin Vuetify

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,27 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloAbes from '@/components/HelloAbes'
-import SecureApi from '@/components/SecureApi'
-import LoginPage from '@/components/LoginPage'
-import RegisterPage from '@/components/RegisterPage'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloAbes',
-      component: HelloAbes
-    },
-    {
-      path: '/secured',
-      name: 'SecureApi',
-      component: SecureApi
-    },
-    {
-      path: '/login',
-      name: 'LoginPage',
-      component: LoginPage
-    },
-    {
-      path: '/register',
-      name: 'RegisterPage',
-      component: RegisterPage
-    }
-  ]
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
 })
+
+export default router
