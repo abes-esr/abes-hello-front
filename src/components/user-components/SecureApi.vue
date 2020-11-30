cb802ad0cfe76a13776bcd8ad20c280fd798a97e
Duy Son Tran <tran@abes.fr>
2020-11-30 17:33:56 +0100

    Ajouter le data table de vuetify

--- src/components/user-components/SecureApi.vue
+++ src/components/user-components/SecureApi.vue
@@ -15,21 +15,41 @@
           <h1 class="mt-10">Bienvenue à l'Abes : {{user}}</h1>
         </v-col>
       </v-row>
+    </div>
 
-      <v-row class="text-center">
+    <div v-for="commande in listCommande" :key="commande.id">
+      <v-row>
         <v-col
           md="6"
           offset-md="3">
+          <div class="text-center">
+            <h3>Commande - {{ commande.id }}</h3>
+            <h3>Fournisseur - {{ commande.fournisseur.name }}</h3>
+          </div>
           <v-data-table
             :headers="tabHeader"
-            :items="produits"
+            :items="commande.products"
+            item-key="price"
             :items-per-page="5"
             class="elevation-1"
-          ></v-data-table>
+          >
+          <template v-slot:item="{ item }">
+            <tr>
+              <td>{{item.name}}</td>
+              <td>{{item.price}}</td>  
+            </tr>
+          </template>
+          <template slot="body.append">
+            <tr class="pink--text">
+                <th class="title">Totals</th>
+                <th class="title">{{ sumField(commande.products,'price') }}</th>
+            </tr>
+          </template>
+          </v-data-table>
+          
         </v-col>
       </v-row>
-
-    </div>
+    </div>  
 
     <div v-if="!loading">
       <v-row class="text-center" >
@@ -61,56 +81,75 @@ export default {
   name: 'SecureApi',
   data: () => ({
     urlApi: 'http://localhost:8081/secured',
+    urlCommande: 'http://localhost:8081/secured/commande',
     resApi: '',
     loading: true,
     tabHeader:[
       {
         text: 'Produit',
-        align: 'start',
+        align: 'left',
         sortable: false,
         value: 'name',
       },
       { 
         text: 'Prix',
-        value: 'prix'
+        value: 'price',
       },
-    ]
-
+      
+    ],
+    listCommande: [],
   }),
   computed: {
     ...mapGetters({
       isLoggedIn: 'auth/isLogged',
       token: 'authToken/authenticated',
-      user: 'auth/user',
-    })
+      user: 'auth/user', 
+    }),
+    
   },
   created () {
     this.checkTokenAction(this.urlApi);
-    setTimeout(async () => {
-      let config = {
-        headers: {
-          Authorization: 'Bearer '+this.token,
-        },
+
+
+    setTimeout( async() => {
+
+      if(this.token !== null ) {
+          let config = {
+          headers: {
+            Authorization: 'Bearer '+this.token,
+          },
+        };
+
+        let apiReq = await this.$http.get(this.urlApi,config);
+        let commandReq = await this.$http.get(this.urlCommande,config)
+        await this.$http.all([apiReq, commandReq])
+          .then(this.$http.spread((first_response, second_response) => {
+            this.loading = false;
+            this.resApi = first_response.data;
+            this.listCommande = second_response.data;     
+          }))
+          .catch(error => {
+            this.resApi = error;
+            this.$router.push({ name: 'LoginPage' });
+            this.loading = false;
+          })
       }
-      await this.$http.get(this.urlApi,config)
-        .then(res => {
-              this.resApi = res.data;
-              this.loading = false;
-          }
-        )
-        .catch(error => {
-          this.resApi = error;
-          this.$router.push({ name: 'LoginPage' });
-          this.loading = false;
-        })
     }, 2000);
+
+    
   },
+
   methods: {
 
     ...mapActions({
       checkTokenAction: 'authToken/sendHeaderToken',
     }),
-
+    
+    sumField(commandeList,key) {
+        // sum data in give key (property)
+        return commandeList.reduce((a, b) => a + (b[key] || 0), 0)
+        
+    }
   },
 }
 </script>
