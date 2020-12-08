05a13a29fe3b04f7ffa3dc7d724f4b823d4dcfff
Duy <56829213+4duytran@users.noreply.github.com>
2020-12-08 18:12:12 +0100

    Update nouveau Jenkinsfile

--- src/components/auth-components/RegisterForm.vue
+++ src/components/auth-components/RegisterForm.vue
@@ -118,7 +118,7 @@ export default {
             .then(res => this.resApi = res.data)
             .catch(err => {
               this.resApi = err;
-              console.log(err.response);
+              console.log(err.response.data);
               });
             this.loading=false;
           }, 2000);
