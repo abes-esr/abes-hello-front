8fa70b04bfc602b64ed6344f24ee2d15b5a8bbd1
Duy <56829213+4duytran@users.noreply.github.com>
2020-12-08 16:27:47 +0100

    Update nouveau Jenkinsfile

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import store from './store'
 
 Vue.config.productionTip = false;
 Vue.prototype.$http = Axios;
-Vue.prototype.$apiUrl = ' https://hello-dev.abes.fr/api/'
+Vue.prototype.$apiUrl = 'https://hello-dev.abes.fr/api/';
 
 new Vue({
   router,
